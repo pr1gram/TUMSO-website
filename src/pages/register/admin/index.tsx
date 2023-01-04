7f1fe038ca19ece883d7eabbb22411e178bda6c7
pryter <peterphongpak@gmail.com>
2023-01-04 16:33:17 +0700

    fix: ui

--- src/pages/register/admin/index.tsx
+++ src/pages/register/admin/index.tsx
@@ -32,8 +32,8 @@ const Page = () => {
 
   return (
     <div className="min-h-screen w-full">
-      <div className="mx-auto flex max-w-[1250px] flex-wrap justify-center space-x-4 p-6 text-gray-800 sm:justify-start">
-        <div className="mt-6">
+      <div className="mx-auto flex max-w-[1670px] flex-wrap justify-center p-6 text-gray-800 sm:justify-start">
+        <div className="mt-6 mr-4">
           <h1 className="mb-1 text-2xl font-semibold text-gray-600">
             Waiting{" "}
             <span className="text-base font-normal text-gray-500">
@@ -81,7 +81,7 @@ const Page = () => {
               })}
           </div>
         </div>
-        <div className="mt-6">
+        <div className="mt-6 mr-4">
           <h1 className="mb-1 text-2xl font-semibold text-green-700">
             Accepted{" "}
             <span className="text-base font-normal text-gray-500">
@@ -129,7 +129,7 @@ const Page = () => {
               })}
           </div>
         </div>
-        <div className="mt-6">
+        <div className="mt-6 mr-4">
           <h1 className="mb-1 text-2xl font-semibold text-red-700">
             Rejected{" "}
             <span className="text-base font-normal text-gray-500">
@@ -172,12 +172,66 @@ const Page = () => {
                         {parseTimestamp(d.timestamp)}
                       </span>
                     </span>
-                    <h1>
-                      Reason:{" "}
-                      <span className="font-medium text-gray-500">
+                    <div className="flex items-center space-x-1">
+                      <h1>Reason: </h1>
+                      <h1 className="max-w-[260px] overflow-x-auto whitespace-nowrap font-medium text-gray-500">
                         {d.reason}
+                      </h1>
+                    </div>
+                  </div>
+                )
+              })}
+          </div>
+        </div>
+        <div className="mt-6 mr-4">
+          <h1 className="mb-1 text-2xl font-semibold text-yellow-700">
+            Editing{" "}
+            <span className="text-base font-normal text-gray-500">
+              - {appData.filter((d) => d.status === "editing").length}{" "}
+              applicants -
+            </span>
+          </h1>
+          <div className="flex max-h-[600px] min-h-[121px] min-w-[387px] flex-col items-center space-y-2 overflow-y-auto rounded-lg border border-gray-400 px-2 py-4">
+            {appData
+              .filter((d) => d.status === "editing")
+              .map((d) => {
+                return (
+                  <div
+                    key={d.id}
+                    onClick={() => {
+                      window.location.replace(
+                        `/register/admin/review?id=${d.id}`
+                      )
+                    }}
+                    className="flex cursor-pointer flex-col rounded-md border border-gray-400 py-2 px-6 text-gray-700"
+                  >
+                    <span className="text-sm text-gray-400">
+                      App id: {d.id}
+                    </span>
+                    <h1>
+                      Subject:{" "}
+                      <span className="font-semibold">
+                        {d.data.selection.subject}
                       </span>
                     </h1>
+                    <h1>
+                      School:{" "}
+                      <span className="font-semibold">
+                        {d.data.school.name}
+                      </span>
+                    </h1>
+                    <span>
+                      Submission date:{" "}
+                      <span className="font-semibold">
+                        {parseTimestamp(d.timestamp)}
+                      </span>
+                    </span>
+                    <div className="flex items-center space-x-1">
+                      <h1>Reason: </h1>
+                      <h1 className="max-w-[260px] overflow-x-auto whitespace-nowrap font-medium text-gray-500">
+                        {d.reason}
+                      </h1>
+                    </div>
                   </div>
                 )
               })}
