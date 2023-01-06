e65117573967e6aa8dac00de1b1bace6661df2af
pryter <peterphongpak@gmail.com>
2023-01-06 17:14:50 +0700

    feat: reduce loading quota

--- src/contexts/admin.tsx
+++ src/contexts/admin.tsx
@@ -1,25 +1,93 @@
+import type { DocumentSnapshot } from "@firebase/firestore"
 import {
   collection,
   doc,
   getDoc,
   getDocs,
   getFirestore,
-  updateDoc
+  limit,
+  orderBy,
+  query,
+  startAfter,
+  updateDoc,
+  where
 } from "@firebase/firestore"
 
 export const useAdminControl = () => {
   const db = getFirestore()
   const submittedCol = collection(db, "submitted")
 
-  const getSubmitted = async () => {
-    const dataCollection = await getDocs(submittedCol)
-
-    return (
-      dataCollection.docs
-        .map((d) => ({ id: d.id, ...d.data() }))
-        // @ts-ignore
-        .sort((a, b) => a.data.school.name.localeCompare(b.data.school.name))
+  const getAdditional = async (
+    last: DocumentSnapshot,
+    status: string
+  ): Promise<[any, any]> => {
+    const addit = await getDocs(
+      query(
+        submittedCol,
+        where("status", "==", status),
+        orderBy("timestamp"),
+        limit(10),
+        startAfter(last)
+      )
     )
+
+    return [
+      addit.docs.map((d) => ({ id: d.id, ...d.data() })),
+      addit.docs[addit.docs.length - 1]
+    ]
+  }
+  const getSubmitted = async (fullData = false): Promise<[any, any]> => {
+    const accepted = await getDocs(
+      query(
+        submittedCol,
+        where("status", "==", "accepted"),
+        orderBy("timestamp"),
+        limit(fullData ? 9999 : 10)
+      )
+    )
+    const waiting = await getDocs(
+      query(
+        submittedCol,
+        where("status", "==", "waiting"),
+        orderBy("timestamp"),
+        limit(fullData ? 9999 : 10)
+      )
+    )
+
+    const rejected = await getDocs(
+      query(
+        submittedCol,
+        where("status", "==", "rejected"),
+        orderBy("timestamp"),
+        limit(fullData ? 9999 : 10)
+      )
+    )
+
+    const editing = await getDocs(
+      query(
+        submittedCol,
+        where("status", "==", "editing"),
+        orderBy("timestamp"),
+        limit(fullData ? 9999 : 10)
+      )
+    )
+
+    const dataCollection = [
+      ...accepted.docs,
+      ...editing.docs,
+      ...rejected.docs,
+      ...waiting.docs
+    ]
+
+    return [
+      dataCollection.map((d) => ({ id: d.id, ...d.data() })),
+      {
+        accepted: accepted.docs[accepted.docs.length - 1],
+        editing: editing.docs[editing.docs.length - 1],
+        rejected: rejected.docs[rejected.docs.length - 1],
+        waiting: waiting.docs[waiting.docs.length - 1]
+      }
+    ]
   }
 
   const getCount = async () => {
@@ -76,6 +144,7 @@ export const useAdminControl = () => {
     getSubmitted,
     loadFormData,
     updateStatus,
-    getCount
+    getCount,
+    getAdditional
   }
 }
--- src/pages/register/admin/index.tsx
+++ src/pages/register/admin/index.tsx
@@ -1,8 +1,10 @@
 import type { DocumentData } from "@firebase/firestore"
 import {
   CheckIcon,
+  ClockIcon,
   ExclamationTriangleIcon,
-  MinusSmallIcon
+  MinusSmallIcon,
+  PencilIcon
 } from "@heroicons/react/20/solid"
 import Router from "next/router"
 import { useEffect, useState } from "react"
@@ -13,11 +15,13 @@ import { parseTimestamp } from "@/utils/time"
 
 const Page = () => {
   const { user } = useFirebaseAuth()
-  const { getSubmitted, getCount } = useAdminControl()
+  const { getSubmitted, getCount, getAdditional } = useAdminControl()
   const [appData, setData] = useState<DocumentData[]>([])
   const [count, setCount] = useState<any>(undefined)
   const [dupedIndex, setDupedIndex] = useState<string[]>([])
   const [indexBySchool, setIndexBySchool] = useState<any>(null)
+  const [fr, setFR] = useState(false)
+  const [latestSnap, setLS] = useState<any>({})
 
   const indexByKey = (samples: any[], collector: (s: any) => string) => {
     const indexedByKey: { [key: string]: any[] } = {}
@@ -31,6 +35,28 @@ const Page = () => {
     })
 
     return indexedByKey
+  }
+
+  const getAdditionalRecords = async (status: string) => {
+    if (!latestSnap[status]) return
+    const lastSnap = latestSnap[status]
+    const [addD, lastSnapD] = await getAdditional(lastSnap, status)
+    setData((prev) => {
+      const mixed = [...prev]
+      addD.forEach((i: any) => {
+        if (!mixed.some((e) => e.id === i.id)) {
+          mixed.push(i)
+        }
+      })
+
+      return mixed.sort((a, b) =>
+        a.data.school.name.localeCompare(b.data.school.name)
+      )
+    })
+    setLS((prev: any) => ({
+      ...prev,
+      [status]: lastSnapD
+    }))
   }
 
   const analyseData = (
@@ -58,10 +84,21 @@ const Page = () => {
     return [dupedList, indexedByPrimary]
   }
 
-  const fetchData = async () => {
-    const data = await getSubmitted()
+  const fetchData = async (full = false) => {
+    const [data, latestSnapshot] = await getSubmitted(full)
+    setLS(latestSnapshot)
+    setData(
+      data.sort((a: any, b: any) =>
+        a.data.school.name.localeCompare(b.data.school.name)
+      )
+    )
+    const ccount = await getCount()
+    setCount(ccount)
+  }
+
+  useEffect(() => {
     const [duped, indexedBySchool] = analyseData(
-      data,
+      appData,
       (a) => a.data.school.name,
       (a) => a.data.selection.subject,
       (a, b) => {
@@ -70,11 +107,7 @@ const Page = () => {
     )
     setIndexBySchool(indexedBySchool)
     setDupedIndex(duped)
-    setData(data)
-    const ccount = await getCount()
-    setCount(ccount)
-  }
-
+  }, [appData])
   useEffect(() => {
     if (user.isLoggedIn() === false) {
       Router.push("/register")
@@ -105,6 +138,47 @@ const Page = () => {
       return <span className="text-orange-600">{v}</span>
     }
     return <span className="text-red-600">{v}</span>
+  }
+
+  const getIndicator = (data: any, subject: string) => {
+    if (data.some((s: any) => s.data.selection.subject === subject)) {
+      const stat = data.filter(
+        (s: any) => s.data.selection.subject === subject
+      )[0].status
+
+      switch (stat) {
+        case "accepted":
+          return (
+            <CheckIcon
+              stroke={"currentColor"}
+              strokeWidth={2.4}
+              className="h-4 w-4 text-green-500"
+            />
+          )
+        case "rejected":
+          return <ExclamationTriangleIcon className="h-4 w-4 text-red-500" />
+        case "waiting":
+          return <ClockIcon className="h-4 w-4 text-gray-600" />
+        case "editing":
+          return <PencilIcon className="h-4 w-4 text-yellow-600" />
+        default:
+          return (
+            <MinusSmallIcon
+              stroke={"currentColor"}
+              strokeWidth={2.4}
+              className="h-4 w-4 text-gray-500"
+            />
+          )
+      }
+    } else {
+      return (
+        <MinusSmallIcon
+          stroke={"currentColor"}
+          strokeWidth={2.4}
+          className="h-4 w-4 text-gray-500"
+        />
+      )
+    }
   }
 
   return (
@@ -147,141 +221,64 @@ const Page = () => {
                 )
               })}
           </div>
-          <h2 className="font-medium text-gray-500">By Schools</h2>
-          <div className="flex h-[80px] items-end text-xs font-semibold">
-            <h1 className="ml-[2px] mr-[11px] w-[10px] -rotate-90">PHYSICS</h1>
-            <h1 className="mr-[10px] w-[10px] -rotate-90">CHEMISTRY</h1>
-            <h1 className="mr-[10px] w-[10px] -rotate-90">BIOLOGY</h1>
-            <h1 className="mr-[10px] w-[10px] -rotate-90">MATHS</h1>
-            <h1 className="w-[10px] -rotate-90">COMPUTER</h1>
-            <h1 className="w-full text-center text-base font-medium">
-              School name
-            </h1>
-          </div>
-          <div className="max-h-[216px] overflow-y-auto border-t border-gray-400">
-            {Object.entries(indexBySchool || {}).map(([key, val], i) => {
-              if (key === "") return <></>
-              return (
+          <h2 className="font-medium text-gray-500">By Schools </h2>
+          <div className="relative">
+            {!fr && (
+              <div className="absolute top-0 left-0 z-[20] flex h-full w-full items-center justify-center rounded-md bg-gray-900 bg-opacity-25 backdrop-blur-sm">
                 <div
-                  className="flex items-center border-b border-gray-400 py-1"
-                  key={`s-${i}`}
+                  onClick={() => {
+                    setFR(true)
+                    fetchData(true)
+                  }}
+                  className="cursor-pointer rounded-md border border-white bg-white bg-opacity-70 py-1.5 px-6 font-medium text-gray-800"
                 >
-                  <div className="mr-2 flex space-x-1">
-                    <h1>
-                      {
-                        // @ts-ignore
-                        val?.some(
-                          (s: any) => s.data.selection.subject === "physics"
-                        ) ? (
-                          <CheckIcon
-                            stroke={"currentColor"}
-                            strokeWidth={2.4}
-                            className="h-4 w-4 text-green-500"
-                          />
-                        ) : (
-                          <MinusSmallIcon
-                            stroke={"currentColor"}
-                            strokeWidth={2.4}
-                            className="h-4 w-4 text-gray-500"
-                          />
-                        )
-                      }
-                    </h1>
-                    <h1>
-                      {
-                        // @ts-ignore
-                        val?.some(
-                          (s: any) => s.data.selection.subject === "chemistry"
-                        ) ? (
-                          <CheckIcon
-                            stroke={"currentColor"}
-                            strokeWidth={2.4}
-                            className="h-4 w-4 text-green-500"
-                          />
-                        ) : (
-                          <MinusSmallIcon
-                            stroke={"currentColor"}
-                            strokeWidth={2.4}
-                            className="h-4 w-4 text-gray-500"
-                          />
-                        )
-                      }
-                    </h1>
-                    <h1>
-                      {
-                        // @ts-ignore
-                        val?.some(
-                          (s: any) => s.data.selection.subject === "biology"
-                        ) ? (
-                          <CheckIcon
-                            stroke={"currentColor"}
-                            strokeWidth={2.4}
-                            className="h-4 w-4 text-green-500"
-                          />
-                        ) : (
-                          <MinusSmallIcon
-                            stroke={"currentColor"}
-                            strokeWidth={2.4}
-                            className="h-4 w-4 text-gray-500"
-                          />
-                        )
-                      }
-                    </h1>
-                    <h1>
-                      {
-                        // @ts-ignore
-                        val?.some(
-                          (s: any) => s.data.selection.subject === "mathematics"
-                        ) ? (
-                          <CheckIcon
-                            stroke={"currentColor"}
-                            strokeWidth={2.4}
-                            className="h-4 w-4 text-green-500"
-                          />
-                        ) : (
-                          <MinusSmallIcon
-                            stroke={"currentColor"}
-                            strokeWidth={2.4}
-                            className="h-4 w-4 text-gray-500"
-                          />
-                        )
-                      }
-                    </h1>
-                    <h1>
-                      {
-                        // @ts-ignore
-                        val?.some(
-                          (s: any) => s.data.selection.subject === "computer"
-                        ) ? (
-                          <CheckIcon
-                            stroke={"currentColor"}
-                            strokeWidth={2.4}
-                            className="h-4 w-4 text-green-500"
-                          />
-                        ) : (
-                          <MinusSmallIcon
-                            stroke={"currentColor"}
-                            strokeWidth={2.4}
-                            className="h-4 w-4 text-gray-500"
-                          />
-                        )
-                      }
-                    </h1>
-                  </div>
-                  <h1 className="max-w-[250px] overflow-x-scroll whitespace-nowrap">
-                    {key}
-                  </h1>
+                  Enable Analytic
                 </div>
-              )
-            })}
+              </div>
+            )}
+            <div className="flex h-[80px] items-end text-xs font-semibold">
+              <h1 className="ml-[2px] mr-[11px] w-[10px] -rotate-90">
+                PHYSICS
+              </h1>
+              <h1 className="mr-[10px] w-[10px] -rotate-90">CHEMISTRY</h1>
+              <h1 className="mr-[10px] w-[10px] -rotate-90">BIOLOGY</h1>
+              <h1 className="mr-[10px] w-[10px] -rotate-90">MATHS</h1>
+              <h1 className="w-[10px] -rotate-90">COMPUTER</h1>
+              <h1 className="w-full text-center text-base font-medium">
+                School name
+              </h1>
+            </div>
+            <div className="max-h-[216px] overflow-y-auto border-t border-gray-400">
+              {Object.entries(indexBySchool || {}).map(
+                ([key, val]: [key: string, val: any], i) => {
+                  if (key === "") return <></>
+                  return (
+                    <div
+                      className="flex items-center border-b border-gray-400 py-1"
+                      key={`s-${i}`}
+                    >
+                      <div className="mr-2 flex space-x-1">
+                        <h1>{getIndicator(val, "physics")}</h1>
+                        <h1>{getIndicator(val, "chemistry")}</h1>
+                        <h1>{getIndicator(val, "biology")}</h1>
+                        <h1>{getIndicator(val, "mathematics")}</h1>
+                        <h1>{getIndicator(val, "computer")}</h1>
+                      </div>
+                      <h1 className="max-w-[250px] overflow-x-scroll whitespace-nowrap">
+                        {key}
+                      </h1>
+                    </div>
+                  )
+                }
+              )}
+            </div>
           </div>
         </div>
         <div className="mt-6 mr-4">
           <h1 className="mb-1 text-2xl font-semibold text-gray-600">
             Waiting{" "}
             <span className="text-base font-normal text-gray-500">
-              - {appData.filter((d) => d.status === "waiting").length}{" "}
-              applicants -
+              - {appData.filter((d) => d.status === "waiting").length} showing -
             </span>
           </h1>
           <div className="flex max-h-[600px] flex-col items-center space-y-2 overflow-y-auto rounded-lg border border-gray-400 px-2 py-4">
@@ -327,14 +324,22 @@ const Page = () => {
                   </div>
                 )
               })}
+            <div
+              className="w-full cursor-pointer rounded-md border border-gray-400 py-1 text-center text-gray-500"
+              onClick={() => {
+                getAdditionalRecords("waiting")
+              }}
+            >
+              Load More
+            </div>
           </div>
         </div>
         <div className="mt-6 mr-4">
           <h1 className="mb-1 text-2xl font-semibold text-green-700">
             Accepted{" "}
             <span className="text-base font-normal text-gray-500">
-              - {appData.filter((d) => d.status === "accepted").length}{" "}
-              applicants -
+              - {appData.filter((d) => d.status === "accepted").length} showing
+              -
             </span>
           </h1>
           <div className="flex max-h-[600px] flex-col items-center space-y-2 overflow-y-auto rounded-lg border border-gray-400 px-2 py-4">
@@ -375,14 +380,22 @@ const Page = () => {
                   </div>
                 )
               })}
+            <div
+              className="w-full cursor-pointer rounded-md border border-gray-400 py-1 text-center text-gray-500"
+              onClick={() => {
+                getAdditionalRecords("accepted")
+              }}
+            >
+              Load More
+            </div>
           </div>
         </div>
         <div className="mt-6 mr-4">
           <h1 className="mb-1 text-2xl font-semibold text-red-700">
             Rejected{" "}
             <span className="text-base font-normal text-gray-500">
-              - {appData.filter((d) => d.status === "rejected").length}{" "}
-              applicants -
+              - {appData.filter((d) => d.status === "rejected").length} showing
+              -
             </span>
           </h1>
           <div className="flex max-h-[600px] min-h-[121px] min-w-[387px] flex-col items-center space-y-2 overflow-y-auto rounded-lg border border-gray-400 px-2 py-4">
@@ -434,14 +447,21 @@ const Page = () => {
                   </div>
                 )
               })}
+            <div
+              className="w-full cursor-pointer rounded-md border border-gray-400 py-1 text-center text-gray-500"
+              onClick={() => {
+                getAdditionalRecords("rejected")
+              }}
+            >
+              Load More
+            </div>
           </div>
         </div>
         <div className="mt-6 mr-4">
           <h1 className="mb-1 text-2xl font-semibold text-yellow-700">
             Editing{" "}
             <span className="text-base font-normal text-gray-500">
-              - {appData.filter((d) => d.status === "editing").length}{" "}
-              applicants -
+              - {appData.filter((d) => d.status === "editing").length} showing -
             </span>
           </h1>
           <div className="flex max-h-[600px] min-h-[121px] min-w-[387px] flex-col items-center space-y-2 overflow-y-auto rounded-lg border border-gray-400 px-2 py-4">
@@ -488,6 +508,14 @@ const Page = () => {
                   </div>
                 )
               })}
+            <div
+              className="w-full cursor-pointer rounded-md border border-gray-400 py-1 text-center text-gray-500"
+              onClick={() => {
+                getAdditionalRecords("editing")
+              }}
+            >
+              Load More
+            </div>
           </div>
         </div>
       </div>
